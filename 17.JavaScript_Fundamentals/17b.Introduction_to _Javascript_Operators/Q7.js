b5e62e074cd6a75e89dd5a56c7295c8da0ef75b4
iLAMBiTCodes <72004959+iLAMBiTCodes@users.noreply.github.com>
2024-02-28 19:21:08 +0530

    Add files via upload

---
+++ 17.JavaScript_Fundamentals/17b.Introduction_to _Javascript_Operators/Q7.js
@@ -0,0 +1,6 @@
+// Program to calculate area of a circle
+
+const radius = 10;
+// Formula for area of a circle
+const Area = (Math.PI) * (radius**2);
+console.log(`Area of circle with radius ${radius} is ${Area}`);
