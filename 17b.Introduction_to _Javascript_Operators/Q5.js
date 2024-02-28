eedd4d1676e469438a4469dbe713633d4346b737
iLAMBiTCodes <72004959+iLAMBiTCodes@users.noreply.github.com>
2024-02-28 19:20:09 +0530

    Delete 17b.Introduction_to _Javascript_Operators directory

--- 17b.Introduction_to _Javascript_Operators/Q5.js
+++
@@ -1,8 +0,0 @@
-// JS Program to calculate Simple interest
- 
-const principal = 1000
-const rate = 10
-const time = 10 //Time in years
-
-const Simple_Interest = (principal * rate * time) / 100
-console.log(`Simple interest is ${Simple_Interest}`)
--- 17b.Introduction_to _Javascript_Operators/Q6.js
+++
@@ -1,8 +0,0 @@
-// BMI Calculator
-
-const weight_in_kgs = 45
-const height_in_meters = 1.62
-
-const BMI = weight_in_kgs / (height_in_meters * height_in_meters)
-
-console.log(`Your BMI in kg/m^2 is ${BMI}`)
